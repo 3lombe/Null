f2aab725337f54fe868fd9e908678fea566bcf01
Nsonsa <elombekisala@gmail.com>
2019-01-11 22:18:48 -0500

    Initial Commit

---
+++ Null/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Null
+//
+//  Created by Nsonsa Elombe Kisala Jr. on 1/11/19.
+//  Copyright © 2019 Nsonsa Kisala Jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
